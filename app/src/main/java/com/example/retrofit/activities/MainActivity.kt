43f1b6bf64a06450dc13caf395d89f267fa58f90
MariaVivancos <mariavivancos@maria.vivancos>
2022-02-03 13:36:37 +0100

    Improved layout

--- app/src/main/java/com/example/retrofit/activities/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.retrofit.R
 import com.example.retrofit.httpRepository.HttpRepository
 import kotlinx.coroutines.CoroutineScope
@@ -21,10 +22,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val randomNumber = (0..385).random()
-        val myText = findViewById<TextView>(R.id.tv_showRequest)
+        //val myText = findViewById<TextView>(R.id.tv_showRequest)
         val name = findViewById<TextView>(R.id.tv_name)
         val description = findViewById<TextView>(R.id.tv_description)
         val image = findViewById<ImageView>(R.id.iv_image)
+
 
         CoroutineScope(Dispatchers.IO).launch { // run in the Background
             val repo = HttpRepository()
@@ -32,8 +34,10 @@ class MainActivity : AppCompatActivity() {
             val entry = repo.getAnEntry(randomNumber)
 
             withContext(Dispatchers.Main) { // run in the foreground (UI Thread)
-                myText.text = "Found ${allEntries?.data?.creatures?.food?.size} and entry called ${entry?.data?.name}"
-
+                //myText.text = "Found ${allEntries?.data?.creatures?.food?.size} and entry called ${entry?.data?.name}"
+                name.text = "${entry?.data?.name}"
+                description.text = "${entry?.data?.description}"
+                Glide.with(this@MainActivity).load("${entry?.data?.image}").into(image)
             }
         }
 
