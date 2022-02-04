5eadf23ba98f877b669d65a631476fff03b5a249
MariaVivancos <mariavivancos@maria.vivancos>
2022-02-04 13:00:58 +0100

    Fixed reload on button click and added placeholder so the reloading doesn't shift the elements.

--- app/src/main/java/com/example/retrofit/activities/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.retrofit.activities
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
@@ -21,29 +22,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val randomNumber = (0..385).random()
-        //val myText = findViewById<TextView>(R.id.tv_showRequest)
         val name = findViewById<TextView>(R.id.tv_name)
         val description = findViewById<TextView>(R.id.tv_description)
         val image = findViewById<ImageView>(R.id.iv_image)
+        val button = findViewById<Button>(R.id.btn_more)
 
+        callMyApi(name, description, image)
+        button.setOnClickListener { callMyApi(name,description,image) }
 
+    }
+
+    private fun callMyApi(
+        name: TextView,
+        description: TextView,
+        image: ImageView
+    ) {
         CoroutineScope(Dispatchers.IO).launch { // run in the Background
             val repo = HttpRepository()
-            val allEntries = repo.getAllEntries()
+            //val allEntries = repo.getAllEntries()
+            val randomNumber = (0..385).random()
             val entry = repo.getAnEntry(randomNumber)
 
             withContext(Dispatchers.Main) { // run in the foreground (UI Thread)
 
                 // Name
                 name.text = "${entry?.data?.name}".uppercase()
-
                 // Description
                 description.text = "${entry?.data?.description}"
                 // Image
-                Glide.with(this@MainActivity).load("${entry?.data?.image}").into(image)
+                Glide.with(this@MainActivity).load("${entry?.data?.image}").placeholder(R.drawable.loading_buffer).into(image)
             }
         }
-
     }
 }
